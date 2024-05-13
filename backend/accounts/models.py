0cca864414751cc713c96c51e66a0f5cfd868815
Juunsik <Juunsik29@gmail.com>
2024-05-13 21:10:39 +0900

    chore: User Custom
    - email 로그인을 위한 User model Custom

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from .managers import UserManager
 
 
-# Create your models here.
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
